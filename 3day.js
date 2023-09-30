1078a1eb22aaeb413b78e49137118ad7227839fd
JEETBHARDWAJ <bhardwajjeet408@gmail.com>
2023-10-01 01:10:31 +0530

    new2

--- 3day.js
+++ 3day.js
@@ -1,2 +1,12 @@
 // stak(Primitive) and Heap(non - Primitive)
+let myyoutubechannel = "codingcenter"
 
+let anotherchannel = myyoutubechannel 
+
+console.log(anotherchannel);
+
+anotherchannel = "chaipercharcha"
+
+
+console.log(anotherchannel);
+console.log(myyoutubechannel);
