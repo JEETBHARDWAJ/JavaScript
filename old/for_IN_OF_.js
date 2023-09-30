4b806be622107603b7c85d0e581c6ebc86fd3028
JEETBHARDWAJ <bhardwajjeet408@gmail.com>
2023-09-30 17:41:49 +0530

    1 day

---
+++ old/for_IN_OF_.js
@@ -0,0 +1,23 @@
+d// for in 
+
+const obj = {
+  harry:90,
+  kaju:86,
+  michael:85,
+  mike:85,
+}
+for(let key in obj)
+{
+  console.log(obj[key])
+}
+
+
+// for of loop
+
+
+let people = ["jeet" , "harry", "some", "one"]
+
+for(let name of people)
+{
+  console.log(name)
+}
